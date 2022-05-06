48b1e4eae5af118a71b5f3e5d2a03189472be615
cskayoshiki <yoshiki-yoshimura@freee.co.jp>
2022-05-06 18:20:10 +0900

    edit機能実装

--- mdn-app-second/src/App.js
+++ mdn-app-second/src/App.js
@@ -26,6 +26,16 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if(id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   const taskList = tasks.map(task => (
     <Todo
       id={task.id}
@@ -34,6 +44,7 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
